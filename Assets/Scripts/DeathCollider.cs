0ca53476c3725ad83cbf8d54486d5b7e68874bc4
Colby Johnson <coldrevolutionsent@gmail.com>
2017-02-07 17:49:13 -0500

    Working on MPMap1

---
+++ Assets/Scripts/DeathCollider.cs
@@ -0,0 +1,29 @@
+﻿// -------------------------------------------------------------------------------------------
+
+// Author: Colby Johnson
+
+// Project: Roll Off!
+
+// Last Updated: 02/07/2017
+
+// Credit: Learn to Code By Making Games - Ben Tristem
+
+// Purpose: A script to identify the death collider
+
+// -------------------------------------------------------------------------------------------
+
+using UnityEngine;
+using System.Collections;
+
+public class DeathCollider : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Player1.cs
@@ -0,0 +1,29 @@
+﻿// -------------------------------------------------------------------------------------------
+
+// Author: Colby Johnson
+
+// Project: Roll Off!
+
+// Last Updated: 02/07/2017
+
+// Credit: Learn to Code By Making Games - Ben Tristem
+
+// Purpose: A script to manage Player 1
+
+// -------------------------------------------------------------------------------------------
+
+using UnityEngine;
+using System.Collections;
+
+public class Player1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
