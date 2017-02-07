1b3ce6ff671b343b711dfec8264f9b1b6fbdc2f8
Colby Johnson <coldrevolutionsent@gmail.com>
2017-02-06 21:51:41 -0500

    Last changes 02/06

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,61 @@
+﻿// -------------------------------------------------------------------------------------------
+
+// Author: Colby Johnson
+
+// Project: Roll Off!
+
+// Last Updated: 02/06/2016
+
+// Credit: Learn to Code By Making Games - Ben Tristem
+
+// Purpose: A script to manage the levels
+
+// -------------------------------------------------------------------------------------------
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	[Range(0.0f, 10.0f)]
+	[SerializeField]
+	public float autoLoadNextLevelAfter;
+	
+	void Start () {
+	
+		if (autoLoadNextLevelAfter == 0) {
+		
+			Debug.Log ("No auto load");
+		
+		} else {
+	
+			Invoke ("LoadNextLevel", autoLoadNextLevelAfter);
+			
+		}
+	
+	}
+
+	public void LoadLevel (string name) {
+	
+		Debug.Log ("New Level load: " + name);
+		SceneManager.LoadScene (name);
+	
+	}
+
+	public void QuitRequest () {
+	
+		Debug.Log ("Quit requested");
+		Application.Quit ();
+	
+	}
+	
+	public void LoadNextLevel () {
+	
+		Debug.Log ("Loading next level");
+		
+		SceneManager.LoadScene(Application.loadedLevel + 1);	
+	
+	}
+
+}
---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,62 @@
+﻿// -------------------------------------------------------------------------------------------
+
+// Author: Colby Johnson
+
+// Project: Roll Off!
+
+// Last Updated: 02/06/2016
+
+// Credit: Learn to Code By Making Games - Ben Tristem
+
+// Purpose: A script to manage the music throughout the levels
+
+// -------------------------------------------------------------------------------------------
+
+using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour {
+
+	public AudioClip[] levelMusicChange;
+	
+	private AudioSource audioSource;
+
+	void Awake () {
+	
+		DontDestroyOnLoad (gameObject);
+	
+	}
+	
+	void Start () {
+	
+		audioSource = GetComponent<AudioSource>();
+		
+//		PlayerPrefsManager.SetMasterVolume(.75f);
+		
+//		audioSource.volume = PlayerPrefsManager.GetMasterVolume();	
+	
+	}
+	
+	void OnLevelWasLoaded (int _level) {
+	
+		AudioClip thisLevelMusic = levelMusicChange[_level];
+	
+		Debug.Log ("Music playing for level: " + thisLevelMusic);
+	
+		if (thisLevelMusic) {
+		
+			audioSource.clip = thisLevelMusic;
+			
+			audioSource.Play();
+		
+		}
+	
+	}
+	
+	public void ChangeVolume (float _volume) {
+	
+		audioSource.volume = _volume;
+	
+	}
+	
+}
