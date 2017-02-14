8c53b01389d9fe4b0722a5f92c2c46b16ae0a92b
Colby Johnson <coldrevolutionsent@gmail.com>
2017-02-13 21:10:54 -0500

    Added power sprites and generator logic

---
+++ Assets/Scripts/Powerup.cs
@@ -0,0 +1,82 @@
+﻿// -------------------------------------------------------------------------------------------
+
+// Author: Colby Johnson
+
+// Project: Roll Off!
+
+// Last Updated: 02/13/2017
+
+// Purpose: A script to manage the powerups
+
+// -------------------------------------------------------------------------------------------
+
+using UnityEngine;
+using System.Collections;
+
+public class Powerup : MonoBehaviour {
+
+	[Range(0f,100f)] [SerializeField] private float moveSpeed = 5.0f;
+
+	[SerializeField] private string subTypeName = "UnknownPickup";
+
+	[Range(0.3f, 5f)] [SerializeField] private float flashDelay = 0.5f;
+
+	[SerializeField] AudioClip pickupSound;
+
+	private Renderer renderer;
+
+	// Use this for initialization
+	void Start () {
+	
+		// Name the collider the subTypeName so we know what the pickup does
+		GetComponent<Collider>().name = subTypeName;
+
+		renderer = GetComponent<Renderer>();
+	
+		// Save memory
+		Destroy (gameObject, 5f);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		transform.position += transform.up * Time.deltaTime * moveSpeed;
+
+		if(Time.fixedTime % flashDelay < .2) {
+		
+			renderer.enabled = false;
+		
+		} else {
+		
+			renderer.enabled = true;
+		}
+		
+	}
+	
+	void OnTriggerEnter (Collider other) {
+	
+		if (other.gameObject.tag == "Player1" || other.gameObject.tag == "Player2") {
+		
+			// Play the destroy sound upon being shot
+			GetComponent<AudioSource>().PlayOneShot(pickupSound);
+			
+			// Destroy the enemy
+			StartDestroy(pickupSound.length);
+		
+		}
+	
+	}
+	
+	void StartDestroy (float timeDelay) {
+		
+		// Disable the renderer and collider so that they are no longer visible/active
+		GetComponent<Renderer>().enabled = false;
+		GetComponent<Collider>().enabled = false;
+		
+		// Start the delayed destroy
+		Destroy (gameObject, timeDelay);
+		
+	}
+
+}
---
+++ Assets/Scripts/PowerupGenerator.cs
@@ -0,0 +1,126 @@
+﻿// -------------------------------------------------------------------------------------------
+
+// Author: Colby Johnson
+
+// Project: Roll Off!
+
+// Last Updated: 02/13/2017
+
+// Purpose: A script to manage the powerup generator
+
+// -------------------------------------------------------------------------------------------
+
+using UnityEngine;
+using System.Collections;
+
+public class PowerupGenerator : MonoBehaviour {
+
+	[SerializeField] GameObject[] powerupTypes;
+
+	[SerializeField] private Vector3 direction;
+
+	private Vector3 initialPos;
+	private Vector3 offset;
+	private float moveTimer;
+	[Range(0.1f,100f)] [SerializeField] private float moveSpeed = 1f;
+
+	[SerializeField] private float spawnTimeMax = 4.0f;
+	[SerializeField] private float spawnTimeMin = 1.0f;
+	
+	private float timeLimit;
+	private float timer;
+	
+	// Use this for initialization
+	void Start () {
+	
+		initialPos = transform.position;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		UpdateTimer();
+		
+		Movement();
+		
+	}
+	
+	// Function to manage the timer
+	void UpdateTimer () {
+	
+		// Add time to the timer
+		timer += Time.deltaTime;
+		
+		// Check to see if it's time to spawn an enemy
+		if (timer >= timeLimit) {
+		
+			// Set the new timeLimit for the next spawn
+			timeLimit = Random.Range (spawnTimeMin, spawnTimeMax);
+			
+			// Reset the timer to zero
+			timer = 0;
+			
+			// Spawn an enemy
+			DoSpawn();
+		
+		}
+	
+	}
+
+	void DoSpawn () {
+	
+		// Choose a random index in the enemy array
+		float random = Random.value;
+		
+		if (random < .45f) {
+		
+			// Tell the player
+			Debug.Log ("The random value is: " + random + ". The powerup spawned is speed.");
+		
+			// Instantiate the random powerup at the position of the generator
+			Instantiate (powerupTypes[0], transform.position, transform.rotation);
+		
+		} else if (.45f <= random && random < .9f) {
+		
+			// Tell the player
+			Debug.Log ("The random value is: " + random + ". The powerup spawned is mass.");
+			
+			// Instantiate the random powerup at the position of the generator
+			Instantiate (powerupTypes[1], transform.position, transform.rotation);
+			
+		} else if (.9f <= random && random <= 1f) {
+		
+			// Tell the player
+			Debug.Log ("The random value is: " + random + ". The powerup spawned is instakill.");
+			
+			// Instantiate the random powerup at the position of the generator
+			Instantiate (powerupTypes[2], transform.position, transform.rotation);
+			
+		}
+	
+	}
+
+	void Movement () {
+	
+		moveTimer += moveSpeed * Time.deltaTime;
+		
+		if (direction.x != 0) {
+			
+			offset.x = Mathf.PingPong(moveTimer, direction.x);
+		
+		} else if (direction.y != 0) {
+			
+			offset.y = Mathf.PingPong(moveTimer, direction.y);
+			
+		} else if (direction.z != 0) {
+			
+			offset.z = Mathf.PingPong(moveTimer, direction.z);
+			
+		}
+		
+		transform.position = initialPos + offset;
+	
+	}
+
+}
